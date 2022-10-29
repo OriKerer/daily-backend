5b6a9b0b128d776277a6de69e2cf409bd3e4dd9c
Ori Kerer <orikerergil@gmail.com>
2022-10-29 21:31:50 +0300

    Initial IaC attempt

---
+++ terraform/versions.tf
@@ -0,0 +1,15 @@
+erraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/google"
+      version = ">= 4.41"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.4"
+    }
+  }
+}
