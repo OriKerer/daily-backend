30ff104fdcba277a3cf782381f79d85a6f9204c5
Ori Kerer <orikerergil@gmail.com>
2022-10-31 16:05:55 +0000

    formatting

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,21 @@
 variable "project_id" {
   type        = string
-  sensitive = true
+  sensitive   = true
   description = "The Google Cloud Project Id"
 }
 
 variable "region" {
-  type    = string
+  type        = string
   description = "Region to deploy regional resources"
 }
 
 variable "artifact_repository" {
-  type = string
+  type        = string
   description = "The name of the artifact to be used for storing container images"
 }
 
 variable "gcs_backend_bucket" {
-  type    = string
-  sensitive = true
+  type        = string
+  sensitive   = true
   description = "The name of the bucket to be used as a terraform backend for remote state"
 }
